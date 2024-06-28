167ad9fbebaa3da675632e1eca6231e605f34088
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-28 16:03:33 +0500

    Instagram Influncers Analysis

--- Instagram_Influncers_Analysis.py
+++ Instagram_Influncers_Analysis.py
@@ -2,10 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset with specified column names and adjust for spaces and quotes
-df = pd.read_csv("D:\DataCamp\Datasets\influncers.csv", skipinitialspace=True, quotechar='"')
-
-# Display the first few rows of the dataset to verify
-print(df.head())
+df = pd.read_csv("D:\\DataCamp\\Datasets\\influncers.csv", skipinitialspace=True, quotechar='"')
 
 # Convert Followers, Authentic engagement, and Engagement avg to numeric
 df['Followers'] = df['Followers'].str.replace('M', 'e6').str.replace('K', 'e3').astype(float)
