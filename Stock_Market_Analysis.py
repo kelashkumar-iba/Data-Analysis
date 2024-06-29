d286aa9b3efbb911cee1675e913bdecaa6c717e5
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-29 17:16:28 +0500

    Stock Market Data Analysis

--- Stock_Market_Analysis.py
+++ Stock_Market_Analysis.py
@@ -21,3 +21,21 @@ plt.ylabel('Closing Price')
 plt.xticks(rotation=45)
 plt.grid(True)
 plt.show()
+
+
+# Select columns for closing prices
+price_columns = [
+    'Natural_Gas_Price', 'Crude_oil_Price', 'Copper_Price', 'Bitcoin_Price', 'Platinum_Price', 
+    'Ethereum_Price', 'S&P_500_Price', 'Nasdaq_100_Price', 'Apple_Price', 'Tesla_Price', 
+    'Microsoft_Price', 'Silver_Price', 'Google_Price', 'Nvidia_Price', 'Berkshire_Price', 
+    'Netflix_Price', 'Amazon_Price', 'Meta_Price', 'Gold_Price'
+]
+
+# Calculate the correlation matrix
+correlation_matrix = stocks_df[price_columns].corr()
+
+# Plot the correlation matrix
+plt.figure(figsize=(12, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+plt.title('Correlation Between Closing Prices of Different Stocks')
+plt.show()
