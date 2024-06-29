7e021fe40f9315d0940175fab42ae9c2678f99d8
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-29 16:57:00 +0500

    Stock Market Data Analysis

--- Stock_Market_Analysis.py
+++ Stock_Market_Analysis.py
@@ -5,16 +5,4 @@ import seaborn as sns
 # Load the dataset
 stocks_df = pd.read_csv("D:\DataCamp\Datasets\stocks.csv")
 
-# Filter data for a specific stock (e.g., 'Apple')
-stock_symbol = 'Apple_Price'
-stock_data = stocks_df[['Date', stock_symbol]]
-
-# Plot closing prices over time
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=stock_data, x='Date', y=stock_symbol)
-plt.title(f'Trend of Closing Prices for Apple')
-plt.xlabel('Date')
-plt.ylabel('Closing Price')
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.show()
+print(stocks_df.shape)
