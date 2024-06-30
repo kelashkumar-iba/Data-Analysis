7d5b175491e77363a670ef93662e0cf59a345dad
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-06-30 11:35:43 +0500

    Healthcare Insurance Analysis

--- Healthcare_Insurance_Analysis.py
+++ Healthcare_Insurance_Analysis.py
@@ -3,14 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the dataset
-movies_df = pd.read_csv("D:\\DataCamp\\Datasets\\insurance.csv")
+healthcare = pd.read_csv("D:\\DataCamp\\Datasets\\insurance.csv")
 
 # Set the style for the plots
 sns.set(style="whitegrid")
 
 # Plot the distribution of ages
 plt.figure(figsize=(10, 6))
-sns.histplot(movies_df['age'], kde=True, bins=20)
+sns.histplot(healthcare['age'], kde=True, bins=20)
 plt.title('Age Distribution')
 plt.xlabel('Age')
 plt.ylabel('Frequency')
@@ -19,7 +19,7 @@ plt.show()
 
 # Plot the relationship between BMI and charges
 plt.figure(figsize=(10, 6))
-sns.scatterplot(x='bmi', y='charges', data=data)
+sns.scatterplot(x='bmi', y='charges', data=healthcare)
 plt.title('BMI vs. Charges')
 plt.xlabel('BMI')
 plt.ylabel('Charges')
