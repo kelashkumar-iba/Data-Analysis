603caaf2429d24eceadfabcccf0ce7d977d932b0
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-07-01 19:19:15 +0500

    Healthcare Insurance Analysis Outputs

--- Healthcare_Insurance_Analysis.py
+++ Healthcare_Insurance_Analysis.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the dataset
-healthcare = pd.read_csv("D:\\DataCamp\\Datasets\\insuriii.csv")
+healthcare = pd.read_csv("D:\\DataCamp\\Datasets\\insurii.csv")
 
 # ---------------------------------------------------
 # 1: What is the distribution of ages in the dataset?
